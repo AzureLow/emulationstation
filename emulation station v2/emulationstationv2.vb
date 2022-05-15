47231bdb67344c87e2e07f8227701e45acd65927
AzureLow <105596638+AzureLow@users.noreply.github.com>
2022-05-15 15:47:17 +0100

    all the raw files
    all the raw files

---
+++ emulation station v2/emulationstationv2.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        System.Diagnostics.Process.Start("C:\Users\Low Zones\Documents\MGBA\mGBA-0.8.1-win64\mGBA.exe")
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        System.Diagnostics.Process.Start("C:\Users\Low Zones\Desktop\kega fusion\Fusion.exe")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        System.Diagnostics.Process.Start("C:\Users\Low Zones\Desktop/Dolphin-x64\Dolphin.exe")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        System.Diagnostics.Process.Start("C:\Users\Low Zones\Documents\Mesen\Mesen.exe")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        System.Diagnostics.Process.Start("C:\Users\Low Zones\Documents\Snes9x\snes9x-x64.exe")
+    End Sub
+End Class
